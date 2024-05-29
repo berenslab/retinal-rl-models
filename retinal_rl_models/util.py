ac4b515565b3f465154a80c6be98a88d6b8b4d04
Fabio Seel <fabio.seel@uni-tuebingen.de>
2024-05-29 10:34:16 +0200

    doc: more explicit assertion error text

--- retinal_rl_models/util.py
+++ retinal_rl_models/util.py
@@ -2,6 +2,7 @@ def assert_list(list_candidate, len_list, dtype=int):
     if isinstance(list_candidate, dtype):
         _list = [list_candidate] * len_list
     else:
-        assert len(list_candidate) == len_list
+        if len(list_candidate) != len_list:
+           raise AssertionError("The length of the list does not match the expected length: "+str(len(list_candidate))+" != " + str(len_list))
         _list = list_candidate
     return _list
